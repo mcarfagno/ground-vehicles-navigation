f97e1769f9dd06e93f2b30af7f93f9d193731e45
mcarfagno <carfagnomarcello@gmail.com>
2024-01-09 19:13:17 +0000

    feat: update MPC params

--- src/mpc_control/include/mpc_control/mpc.hpp
+++ src/mpc_control/include/mpc_control/mpc.hpp
@@ -11,23 +11,33 @@
 
 namespace mpc {
 
+static const char TRAJECTORY_DICT_KEY[] = "ref_trajectory";
+static const char INITIAL_STATE_DICT_KEY[] = "x_initial_condition";
+static const char INITIAL_CONTROL_DICT_KEY[] = "u_initial_condition";
+static const char OPTIMIZED_CONTROL_DICT_KEY[] = "u_optimized";
+
 struct MpcParameters {
+  // Mpc problem settings
   std::size_t N = 10;
   double DT = 0.2;
-  double wheel_base = 1.5213;
-  double v_min = 0.0;
-  double v_max = 20.0;
-  double a_min = -3.0;
-  double a_max = 2.0;
-  double steer_min = -0.5;
-  double steer_max = 0.5;
-  double jerk_min = -1.5;
-  double jerk_max = 1.5;
-  double steer_rate_min = -0.5;
-  double steer_rate_max = 0.5;
-  double obstacle_avoidance_dist = 0.5;
+  double obstacle_avoidance_dist = 0.5; // [m]
   std::vector<double> state_error_weights = {1.0, 1.0, 1.0, 0.1};
   std::vector<double> control_rate_weights = {10.0, 100.0};
+
+  // Vehicle Kin constrains
+  double wheel_base = 1.76;     //[m]
+  double v_min = 0.0;           // [m/s]
+  double v_max = 20.0;          // [m/s]
+  double a_min = -3.0;          // [m/ss]
+  double a_max = 2.0;           // [m/ss]
+  double jerk_min = -1.5;       // [m/sss]
+  double jerk_max = 1.5;        // [m/sss]
+  double steer_min = -0.61;     // [rad]
+  double steer_max = 0.61;      // [rad]
+  double steer_rate_min = -0.5; // [rad/s]
+  double steer_rate_max = 0.5;  // [rad/s]
+
+  // IPOPT Settings
   double tol = 1e-3;
   double max_cpu_time = 0.5;
   int max_iter = 200;
@@ -45,8 +55,8 @@ class KinematicMpc {
 private:
   float dt_;
   std::size_t N_;
-  const std::size_t nx_ = 4;
-  const std::size_t nu_ = 2;
+  const std::size_t nx_ = 4; // [x,y,yaw,v]
+  const std::size_t nu_ = 2; // [a, steer]
 
   // Problem placeholder obj
   casadi::Opti opti_;
