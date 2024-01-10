b4917f4cdb3ed6a52b2646049dc2cadd76885475
mcarfagno <carfagnomarcello@gmail.com>
2024-01-10 18:00:50 +0100

    wip: mpc ros node

--- src/mpc_control/include/mpc_control/mpc_node.hpp
+++ src/mpc_control/include/mpc_control/mpc_node.hpp
@@ -3,18 +3,40 @@
 
 #include "mpc_control/mpc.hpp"
 #include <memory>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/Path.h>
+#include <optional>
 #include <ros/ros.h>
+#include <tf/transform_datatypes.h>
+#include <utility>
+#include <vector>
+#include <vision_msgs/Detection3DArray.h>
 
 namespace mpc {
-// TODO(marcello): mpc ROS node
 class MpcNode {
 public:
-  MpcNode(){};
+  MpcNode();
   ~MpcNode(){};
   void run();
 
 private:
   ros::NodeHandle nh_;
+  ros::NodeHandle private_nh_;
+
+  ros::Publisher ctrl_pub_;
+  ros::Publisher mpc_viz_pub_;
+
+  ros::Subscriber odom_sub_;
+  ros::Subscriber path_sub_;
+  ros::Subscriber obstacles_sub_;
+
+  float rate_;
+  std::optional<KinematicMpc> mpc_;
+  std::optional<nav_msgs::Odometry> latest_odom_;
+  std::optional<nav_msgs::Path> path_;
+  std::optional<vision_msgs::Detection3DArray> obstacles_;
+
+  KinematicMpc setup_mpc() const;
 };
 
 } // namespace mpc
