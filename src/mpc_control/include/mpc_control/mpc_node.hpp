c0734d4f9ea40990b089dcc393d10e9d2ed03adf
mcarfagno <carfagnomarcello@gmail.com>
2024-01-10 20:37:26 +0000

    feat: refacctor ROS->casadi conversions

--- src/mpc_control/include/mpc_control/mpc_node.hpp
+++ src/mpc_control/include/mpc_control/mpc_node.hpp
@@ -31,12 +31,22 @@ private:
   ros::Subscriber obstacles_sub_;
 
   float rate_;
+  std::size_t mpc_horizon_steps_;
+  KinematicMpcCmd prev_cmd_;
   std::optional<KinematicMpc> mpc_;
   std::optional<nav_msgs::Odometry> latest_odom_;
   std::optional<nav_msgs::Path> path_;
   std::optional<vision_msgs::Detection3DArray> obstacles_;
 
-  KinematicMpc setup_mpc() const;
+  casadi::DM odometry_to_casadi(const nav_msgs::Odometry &odom) const;
+  casadi::DM cmd_to_casadi(const MpcCmd &cmd) const;
+  casadi::DM path_to_casadi(const nav_msgs::Path &path) const;
+  casadi::DM
+  obstacles_to_casadi(const vision_msgs::Detection3DArray &obs) const;
+  MpcCmd casadi_to_cmd(casadi::DM &in) const;
+
+  void publish_mpc_cmd(double speed, double steer);
+  void publish_rviz_markers(const casadi::DM &predicted_state_traj);
 };
 
 } // namespace mpc
--- src/mpc_control/src/mpc_node.cpp
+++ src/mpc_control/src/mpc_node.cpp
@@ -9,9 +9,11 @@ MpcNode::MpcNode() : private_nh_("~") {
   obstacles_ = std::nullopt;
   mpc_ = std::nullopt;
   latest_odom_ = std::nullopt;
+  prev_cmd_ = {0.0, 0.0};
 
   // params
   private_nh_.param("rate", rate_, double(10.0));
+  private_nh_.param("control_horizon_len", mpc_horizon_steps_, std::size_t(10));
 
   // publishers
   ctrl_pub_ =
@@ -30,10 +32,6 @@ MpcNode::MpcNode() : private_nh_("~") {
       [&](const vision_msgs::Detection3DArrayPtr &msg) { obstacles_ = *msg; });
 }
 
-KinematicMpc MpcNode::setup_mpc() const {
-  // TODO: create MPC
-}
-
 void MpcNode::run() {
   ROS_INFO("MPC controller node start");
   ros::Rate loop_rate(rate_);
@@ -41,19 +39,115 @@ void MpcNode::run() {
     ros::spinOnce();
 
     // check for topics and create mpc instance
-    if (!obstacles_ || !path_ || !latest_odom_) {
+    if (!obstacles_.has_value() || !path_.has_value() ||
+        !latest_odom_.has_value()) {
       ROS_WARN("MPC waiting for necessary topics.");
       loop_rate.sleep();
       continue;
     }
 
-    if (!mpc) {
+    if (!mpc_.has_value()) {
       ROS_INFO("Creating CasADi problem instance");
-      mpc = setup_mpc();
+      model = KinematicModel();
+      params = MpcParameters();
+      params.DT = rate_;
+      params.N = mpc_horizon_steps_;
+      mpc_ = KinematicMpc(model, params);
     }
 
     // control loop
-    // TODO: MPC LOOP
+    auto mpc_dict_in = casadi::DMDict();
+    mpc_dict_in[INITIAL_STATE_DICT_KEY] = odom_to_casadi(latest_odom_.value());
+    mpc_dict_in[INITIAL_CONTROL_DICT_KEY] = cmd_to_casadi(prev_cmd_);
+    mpc_dict_in[TRAJECTORY_DICT_KEY] = (mpc_dict_in, path_.value());
+
+    auto result = mpc_->solve(mpc_dict_in);
+    if (result.has_value()) {
+      auto ctrl =
+          mpc_->casadi_to_cmd(result.value()[OPTIMIZED_CONTROL_DICT_KEY]);
+      publish_mpc_cmd(latest_state_.value().speed +
+                          ctrl.acceleration * control_time_step_,
+                      ctrl.steer);
+      publish_rviz_markers(result.value()[OPTIMIZED_TRAJECTORY_DICT_KEY]);
+      prev_cmd_ = ctrl;
+    } else {
+      publish_mpc_cmd(0.0, 0.0);
+      prev_cmd_ = {0.0, 0.0};
+    }
   }
 }
+
+void MpcNode::publish_mpc_cmd(double speed, double steer) {
+  auto cmd_msg = ackermann_msgs::AckermannDrive();
+  cmd_msg->header.stamp = this->get_clock()->now();
+  cmd_msg->speed = speed;
+  cmd_msg->steering_angle = steer;
+  command_pub_.publish(cmd_msg);
+}
+
+void MpcNode::publish_rviz_markers(const casadi::DM &predicted_state_traj) {
+  auto marker_arr = visualization_msgs::MarkerArray > ();
+
+  // 1- publish optimized state x and y
+  visualization_msgs::msg::Marker marker;
+  marker.header.frame_id = "world";
+  marker.header.stamp = this->get_clock()->now();
+  marker.ns = "mpc_path_marker";
+  marker.id = 0;
+  marker.type = visualization_msgs::msg::Marker::LINE_STRIP;
+  marker.action = visualization_msgs::msg::Marker::ADD;
+  marker.scale.x = 0.2;
+  marker.color.r = 1.0;
+  marker.color.g = 0.0;
+  marker.color.b = 0.0;
+  marker.color.a = 1.0;
+  marker.frame_locked = true;
+
+  for (std::size_t i = 0;
+       i < static_cast<std::size_t>(predicted_state_traj.size1()); i++) {
+    geometry_msgs::msg::Point point;
+    point.x = predicted_state_traj(i, 0).scalar();
+    point.y = predicted_state_traj(i, 1).scalar();
+    marker.points.push_back(point);
+  }
+  marker_arr->markers.push_back(marker);
+  viz_pub_->publish(std::move(marker_arr));
+}
+
+casadi::DM MpcNode::odometry_to_casadi(const nav_msgs::Odometry &odom) const {
+  return casadi::DM(
+      {odom.pose.pose.position.x, odom.pose.pose.position.y,
+       tf::getYaw(odom.pose.pose.orientation),
+       std::hypot(odom.twist.twist.linear.x, odom.twist.twist.linear.y)});
+}
+
+casadi::DM MpcNode::cmd_to_casadi(const MpcCmd &cmd) const {
+  return casadi::DM({cmd.acceleration, cmd.steer});
+}
+
+casadi::DM MpcNode::path_to_casadi(const nav_msgs::Path &path) const {
+  auto tmp = casadi::DM(path.poses.size(), nx_);
+  for (std::size_t i = 0; i < path.poses.size(); i++) {
+    tmp(i, casadi::Slice()) = {
+        path.poses[i].pose.position.x, path.poses[i].pose.position.y,
+        tf::getYaw(path.poses[i].pose.orientation), MPC_REF_SPEED};
+  }
+  return tmp;
+}
+
+casadi::DM
+MpcNode::obstacles_to_casadi(const vision_msgs::Detection3DArray &obs) const {
+  auto tmp = casadi::DM(obs.detections.size(), 2);
+  for (std::size_t i = 0; i < obs.detections.size(); i++) {
+    tmp(i, casadi::Slice()) = {
+        obs.detections[i].results.front().pose.pose.position.x,
+        obs.detections[i].results.front().pose.pose.position.y};
+  }
+  return tmp;
+}
+
+MpcCmd casadi_to_cmd(casadi::DM &in) const {
+  return MpcCmd(in(0, 0).scalar(), in(0, 1).scalar());
+}
+
 } // namespace mpc
