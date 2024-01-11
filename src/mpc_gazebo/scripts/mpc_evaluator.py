ea2489ce0daa7f00681fce4d86c584d33c869594
mcarfagno <carfagnomarcello@gmail.com>
2024-01-11 21:21:48 +0100

    fix script

--- src/mpc_gazebo/scripts/mpc_evaluator.py
+++ src/mpc_gazebo/scripts/mpc_evaluator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from visualization_msgs.msg import Marker, MarkerArray
-from nav_msgs.msg import Odometry,Path
+from nav_msgs.msg import Odometry, Path
 from geometry_msgs.msg import PoseStamped
 from vision_msgs.msg import Detection3D, Detection3DArray, ObjectHypothesisWithPose
 
@@ -19,6 +19,7 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 WORLD_FRAME_ID = "world"
 OBS_RADIUS = 0.5
 
+
 def dist(p1, p2):
     return round(np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2), 3)
 
@@ -32,22 +33,9 @@ def find_angle(v1, v2):
 def parse_csv(filename):
     with open(filename) as f:
         data = [tuple(line) for line in csv.reader(f)]
-        data = list(map(lambda x: [float(y) for y in x],data))
 
-    return data
+    return np.array
 
-def compute_cte(curr_x, curr_y, curr_yaw):
-    dx = [front_x - x for x in self.waypoints[:, 0]]
-    dy = [front_y - y for y in self.waypoints[:, 1]]
-    target_index = int(np.argmin(np.hypot(dx, dy)))
-
-    front_axle_vec_rot_90 = np.array(
-        [[math.cos(curr_yaw - math.pi / 2.0)], [math.sin(curr_yaw - math.pi / 2.0)]]
-    )
-
-    vec_target_2_front = np.array([[dx[target_index]], [dy[target_index]]])
-
-    return np.dot(vec_target_2_front.T, front_axle_vec_rot_90)
 
 class MpcEvaluator(object):
     def __init__(self):
@@ -56,13 +44,13 @@ class MpcEvaluator(object):
 
         rospack = rospkg.RosPack()
         mpc_gazebo_dir = rospack.get_path("mpc_gazebo")
-        self.waypoints = parse_csv(
-            os.path.join(mpc_gazebo_dir, "./data/waypoints.csv")
+        self.waypoints = np.genfromtxt(
+            os.path.join(mpc_gazebo_dir, "./data/waypoints.csv"), delimiter=","
         )
-        self.obstacles = parse_csv(
-            os.path.join(mpc_gazebo_dir, "./data/obstacles.csv")
+        self.obstacles = np.genfromtxt(
+            os.path.join(mpc_gazebo_dir, "./data/obstacles.csv"), delimiter=","
         )
-        
+
         self.broadcaster = tf.TransformBroadcaster()
         self.odometry_sub = rospy.Subscriber(
             "/gem/base_footprint/odom", Odometry, self.odom_cb, queue_size=1
@@ -84,16 +72,24 @@ class MpcEvaluator(object):
                 msg.pose.pose.orientation.w,
             ]
         )
+        speed = np.hypot(msg.twist.twist.linear.x, msg.twist.twist.linear.y)
+
         self.mpc_pos_log.append(
-            (msg.pose.pose.position.x, msg.pose.pose.position.y, yaw)
+            (msg.pose.pose.position.x, msg.pose.pose.position.y, yaw, speed)
         )
 
         # Broadcast latest world -> base_link tf to simulate localization stack
-        self.broadcaster.sendTransform((msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z),
-          tf.transformations.quaternion_from_euler(0, 0, yaw),
-          rospy.Time.now(),
-          msg.child_frame_id,
-          msg.header.frame_id)
+        self.broadcaster.sendTransform(
+            (
+                msg.pose.pose.position.x,
+                msg.pose.pose.position.y,
+                msg.pose.pose.position.z,
+            ),
+            tf.transformations.quaternion_from_euler(0, 0, yaw),
+            rospy.Time.now(),
+            msg.child_frame_id,
+            msg.header.frame_id,
+        )
 
     def publish_path(self):
         msg = Path()
@@ -119,6 +115,8 @@ class MpcEvaluator(object):
 
         for idx, o in enumerate(self.obstacles):
             d = Detection3D()
+            # NOTE: obstacles have the form x,y,radius
+            # I am misusing the bbox size for the radius
             d.bbox.center.position.x = o[0]
             d.bbox.center.position.y = o[1]
             d.bbox.size.x = o[2]
@@ -136,9 +134,9 @@ class MpcEvaluator(object):
             marker.action = 0
             marker.id = idx
             marker.ns = "mpc/obs"
-            marker.scale.x = o[2]*2
-            marker.scale.y = o[2]*2
-            marker.scale.z = 2.0
+            marker.scale.x = o[2] * 2
+            marker.scale.y = o[2] * 2
+            marker.scale.z = 1.0
 
             marker.color.r = 0.0
             marker.color.g = 1.0
@@ -146,11 +144,28 @@ class MpcEvaluator(object):
             marker.color.a = 0.6
             marker.pose.position.x = o[0]
             marker.pose.position.y = o[1]
-            marker.pose.position.z = marker.scale.z*0.5
+            marker.pose.position.z = marker.scale.z * 0.5
 
             msg_viz.markers.append(marker)
         self.obstacles_viz_pub.publish(msg_viz)
 
+    def get_nearest_index(self, curr_x, curr_y):
+        dx = [curr_x - x for x in self.waypoints[:, 0]]
+        dy = [curr_y - y for y in self.waypoints[:, 1]]
+        return int(np.argmin(np.hypot(dx, dy)))
+
+    def compute_cte(self, curr_x, curr_y, curr_yaw):
+        target_index = self.get_nearest_index(curr_x, curr_y)
+        dx = curr_x - self.waypoints[target_index, 0]
+        dy = curr_y - self.waypoints[target_index, 1]
+
+        front_axle_vec_rot_90 = np.array(
+            [[math.cos(curr_yaw - math.pi / 2.0)], [math.sin(curr_yaw - math.pi / 2.0)]]
+        )
+
+        vec_target_2_front = np.array([dx, dy])
+
+        return np.dot(vec_target_2_front.T, front_axle_vec_rot_90)
 
     # TODO: plot MPC results at the end of trial
     def plot(self):
@@ -158,7 +173,24 @@ class MpcEvaluator(object):
 
     def run(self):
         while not rospy.is_shutdown():
-            # TODO: check for goal and plot results
+            # check for goal and plot results
+
+            if len(self.mpc_pos_log) > 0:
+                nearest = self.get_nearest_index(
+                    self.mpc_pos_log[-1][0], self.mpc_pos_log[-1][1]
+                )
+                cte = self.compute_cte(
+                    self.mpc_pos_log[-1][0],
+                    self.mpc_pos_log[-1][1],
+                    self.mpc_pos_log[-1][2],
+                )
+                rospy.loginfo(f"nearest: {nearest} error: {cte}")
+                if nearest == self.waypoints.shape[0] - 1:
+                    rospy.loginfo(f"Goal Reached")
+                    # TODO: plot
+
+                    rospy.loginfo(f"Saving Image to")
+                    rospy.shutdown()
 
             self.publish_path()
             self.publish_obstacles()
